14310937214029a80d1fede8cf5b99d7a80c3057
Antônio Neto <acm.neto1999@gmail.com>
2023-07-28 17:23:56 -0300

    :package: build: adding 'php-restclient' dependence

---
+++ blog/protected/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '93cd790f8a3a0ef565a399d73bcd2f7272ff2050',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '93cd790f8a3a0ef565a399d73bcd2f7272ff2050',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tcdent/php-restclient' => array(
+            'pretty_version' => '0.1.8.3',
+            'version' => '0.1.8.3',
+            'reference' => 'abd13afb7c6cab622e86b6101180f277767d8d83',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tcdent/php-restclient',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
